d26af378010f862591d110259569af32619503d3
AlarconCode <julian.alarcon81@gmail.com>
2023-03-13 19:54:58 +0100

    creacion de footer, pagina de carga y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'loading', component: LoadingPageComponent },
+  {path: 'loading', redirectTo: 'bienvenida'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'publicar', component: PostPageComponent},
+  {path: 'mi-perfil', component: ProfilePageComponent},
+  {path: 'bienvenida', component: WelcomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
