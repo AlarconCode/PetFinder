1635272c9e5e36d7726670f7a38e0f8ac0e640c8
melendezcabreram <melendezcabreram@gmail.com>
2023-03-15 18:04:24 +0100

    header falta el icono

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PostPageComponent } from './pages/post-page/post-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'publicar', component: PostPageComponent},
   {path: 'mi-perfil', component: ProfilePageComponent},
+  {path: 'login', component: LoginPageComponent}
 ];
 
 @NgModule({
