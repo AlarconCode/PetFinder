7ff69faf75c5ea8aa0c3e57287e35111c2ccb3b0
AlarconCode <julian.alarcon81@gmail.com>
2023-03-16 14:30:36 +0100

    creacion maquetacion de pagina de perfil publico

--- src/app/pages/public-profile-page/public-profile-page.component.ts
+++ src/app/pages/public-profile-page/public-profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-profile-page',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./public-profile-page.component.css']
 })
 export class PublicProfilePageComponent {
+
+  constructor(private router:Router) {}
+  
+  goToContactForm() {
+    this.router.navigateByUrl('contacto')
+  }
 
 }
