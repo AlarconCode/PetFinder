065410b75c061ff66a0392633888e3247d598a80
melendezcabreram <melendezcabreram@gmail.com>
2023-03-16 18:46:50 +0100

    primer commit

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2'
+
 
 
 @Component({
@@ -13,27 +13,4 @@ export class ContactPageComponent {
   constructor(private router:Router) {}
 
 
-  buttonSendMailContact() {
-    
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-      },
-      color: '#16697A'
-    })
-    
-    Toast.fire({
-      icon: 'success',
-      title: 'Enviado correctamente'
-    })
-
-    this.router.navigateByUrl('/home')
-
-  }
 }
