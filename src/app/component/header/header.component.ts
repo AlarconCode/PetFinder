e7765e066702f7c63dee4504b2d67e0e1258ac2d
AlarconCode <julian.alarcon81@gmail.com>
2023-03-17 14:27:07 +0100

    header con modal Swal

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -14,9 +14,37 @@ export class HeaderComponent {
     this.status = false
   }
   
-  goToFilter() {
+  async goToFilter() {
     
     this.status = !this.status
+    const { value } = await Swal.fire({
+      title: 'Seleccione Provincia',
+      input: 'select',
+      inputOptions: {
+        'Castilla la Mancha': {
+          1: 'Toledo',
+          2: 'Cuenca',
+          3: 'Albacete',
+          4: 'Ciudad Real',
+          5: 'Guadalajara'
+        },
+        'Madrid': {
+          1: 'Madrid'
+        },
+        'Andalucía': {
+          1: 'Sevilla',
+          2: 'Granada',
+          3: 'Malaga',
+          4: 'Cadiz',
+          5: 'Córdoba',
+          6: 'Huelva',
+          7: 'Almería',
+          8: 'Jaén'
+        }
+      },
+      inputPlaceholder: 'Seleccione provincia',
+      showCancelButton: true,
+    })
 
   }
 
