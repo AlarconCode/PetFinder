7ce0e4f72b0e54a8c5e03a0b7edeea6a6ff4b6e1
AlarconCode <julian.alarcon81@gmail.com>
2023-03-20 18:09:27 +0100

    correcciones css en iconos de vista mi perfil

--- src/app/component/card-post/card-post.component.ts
+++ src/app/component/card-post/card-post.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardPost } from 'src/app/models/card-post';
 import { PostCardService } from 'src/app/shared/post-card.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-card-post',
@@ -38,7 +39,44 @@ export class CardPostComponent {
   }
 
   deletePost() {
-    confirm('desea borrar la publicación')
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: '¿Estas Seguro?',
+      text: "No se podrá deshacer",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, ¡Eliminarlo!',
+      cancelButtonText: 'No, ¡Cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          '¡Eliminado!',
+          'Tu post ha sido eliminado',
+          'success'
+        )
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'Tu post sigue publicado',
+          'error'
+        )
+      }
+    })
+  }
+
+  editPost() {
+    this.router.navigateByUrl('/actualizar-publicacion')
   }
 
 }
