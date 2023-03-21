1402e354b25c772f50b6f75fd64e785939ae5dcf
melendezcabreram <melendezcabreram@gmail.com>
2023-03-21 20:47:02 +0100

    modal puesto

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
@@ -21,7 +22,25 @@ export class LoginPageComponent {
   login(){
     console.log(this.email);
     console.log(this.password);
-    this.router.navigateByUrl('/home')
+    if(1==1){
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Logueado correctamente',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.router.navigateByUrl('/home')
+    }else{
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Usuario no existe',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+    
   }
 
 }
