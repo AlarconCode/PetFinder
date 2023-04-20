d38a86e1a931aed91e75f919928cb1e995a7bc87
melendezcabreram <melendezcabreram@gmail.com>
2023-04-20 16:20:45 +0200

    arreglado

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -20,4 +20,12 @@ export class FooterComponent {
     }
   }
 
+  goToPostForm(){
+    if (this.userService.logueado) {
+      this.router.navigateByUrl('/publicar')
+    } else {
+      this.router.navigateByUrl('/registro')
+    }
+  }
+
 }
